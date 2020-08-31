137e3c043b2e7dfbed3e4c25d0f4a243f28419fc
yohamc <yohanamonforte629@hotmail.com>
2020-08-31 23:44:30 +0000

    primer commit

---
+++ UI5/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("UI5.UI5.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
